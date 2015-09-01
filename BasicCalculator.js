8f23b0af5943635a357300fcb623c19b2d52fd78
Jeff Worley <jeffworley@Davids-MacBook-Pro.local>
2015-09-01 17:40:09 -0400

    Adjusted console.assert statements to remove unnecessary comma's

--- BasicCalculator.js
+++ BasicCalculator.js
@@ -1,13 +1,13 @@
-console.assert(add(0, 0), === 0);
-console.assert(add(0, 1), === 1);
-console.assert(add(0, 2), === 2);
-console.assert(add(0, 3), === 3);
-console.assert(add(0, 4), === 4);
-console.assert(add(0, 5), === 5);
-console.assert(add(0, 6), === 6);
-console.assert(add(0, 7), === 7);
-console.assert(add(0, 8), === 8);
-console.assert(add(0, 9), === 9);
+console.assert(add(0, 0) === 0);
+console.assert(add(0, 1) === 1);
+console.assert(add(0, 2) === 2);
+console.assert(add(0, 3) === 3);
+console.assert(add(0, 4) === 4);
+console.assert(add(0, 5) === 5);
+console.assert(add(0, 6) === 6);
+console.assert(add(0, 7) === 7);
+console.assert(add(0, 8) === 8);
+console.assert(add(0, 9) === 9);
 
 /**
  * @param {Number} A to add to B
@@ -18,16 +18,16 @@ function add(A, B){
   return A + B;
 }; // END ADD
 
-console.assert(minus(0, 0), === 0);
-console.assert(minus(1, 0), === 1);
-console.assert(minus(2, 0), === 2);
-console.assert(minus(3, 0), === 3);
-console.assert(minus(4, 0), === 4);
-console.assert(minus(5, 0), === 5);
-console.assert(minus(6, 0), === 6);
-console.assert(minus(7, 0), === 7);
-console.assert(minus(8, 0), === 8);
-console.assert(minus(9, 0), === 9);
+console.assert(minus(0, 0) === 0);
+console.assert(minus(1, 0) === 1);
+console.assert(minus(2, 0) === 2);
+console.assert(minus(3, 0) === 3);
+console.assert(minus(4, 0) === 4);
+console.assert(minus(5, 0) === 5);
+console.assert(minus(6, 0) === 6);
+console.assert(minus(7, 0) === 7);
+console.assert(minus(8, 0) === 8);
+console.assert(minus(9, 0) === 9);
 
 /**
  * @param {Number} A to be subtracted from by B
@@ -39,16 +39,16 @@ function minus(A, B) {
   return A - B;
 }; //END MINUS
 
-console.assert(times(0, 0), === 0);
-console.assert(times(1, 1), === 1);
-console.assert(times(2, 1), === 2);
-console.assert(times(3, 1), === 3);
-console.assert(times(4, 1), === 4);
-console.assert(times(5, 1), === 5);
-console.assert(times(6, 1), === 6);
-console.assert(times(7, 1), === 7);
-console.assert(times(8, 1), === 8);
-console.assert(times(9, 1), === 9);
+console.assert(times(0, 0) === 0);
+console.assert(times(1, 1) === 1);
+console.assert(times(2, 1) === 2);
+console.assert(times(3, 1) === 3);
+console.assert(times(4, 1) === 4);
+console.assert(times(5, 1) === 5);
+console.assert(times(6, 1) === 6);
+console.assert(times(7, 1) === 7);
+console.assert(times(8, 1) === 8);
+console.assert(times(9, 1) === 9);
 
 /**
  * @param {Number} A to multiply by B
@@ -60,16 +60,16 @@ console.assert(times(9, 1), === 9);
    return A * B;
  }; //END TIMES
 
- console.assert(divide(0, 0), === 0);
- console.assert(divide(1, 1), === 1);
- console.assert(divide(2, 1), === 2);
- console.assert(divide(3, 1), === 3);
- console.assert(divide(4, 1), === 4);
- console.assert(divide(5, 1), === 5);
- console.assert(divide(6, 1), === 6);
- console.assert(divide(7, 1), === 7);
- console.assert(divide(8, 1), === 8);
- console.assert(divide(9, 1), === 9);
+ console.assert(divide(0, 0) === 0);
+ console.assert(divide(1, 1) === 1);
+ console.assert(divide(2, 1) === 2);
+ console.assert(divide(3, 1) === 3);
+ console.assert(divide(4, 1) === 4);
+ console.assert(divide(5, 1) === 5);
+ console.assert(divide(6, 1) === 6);
+ console.assert(divide(7, 1) === 7);
+ console.assert(divide(8, 1) === 8);
+ console.assert(divide(9, 1) === 9);
 
  /**
   * @param {Number} A to be divided by B
