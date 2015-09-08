84d7ae8d2b0dde62dfb4e98d5e404d4e4288cbf8
jeffworley <sirwhirls@gmail.com>
2015-09-08 07:22:23 -0400

    Completed EvenFibonaccis.js, finished code but could pass 3s-and-5s.js and attempted ROT13.js but didn't understand what I was doing so added notes to explain thought process

--- ROT13.js
+++ ROT13.js
@@ -1,3 +1,6 @@
+var test = require('mocha').it,
+  expect = require('chai').expect;
+
 /**
  * The function `reverse` computes the reversal
  * of a given `String` (don't copy-pasta!)
@@ -6,13 +9,19 @@
  * @return {String}
  */
 function reverse(S){
-    // YOUR CODE HERE: NO INTERNET COPY-PASTA!
-    var revResult = '';
-    for (var i = S.length - 1; i >= 0; i--) {
-      revResult += S[i];
-    }
-    return revResult;
+  // YOUR CODE HERE: NO INTERNET COPY-PASTA!
+  // Solved by iterating through the string given by each letters location.
+/*  var revResult = '';
+  for (var i = S.length - 1; i >= 0; i--) {
+    revResult += S[i];
+  }
+  return revResult;
+}*/
+
+  // Solved smarter by using JavaScript built in methods
+  return S.split('').reverse().join('');
 }
+
 
 console.assert(reverse("") === ""); // Really?
 
@@ -43,9 +52,21 @@ console.assert(
  * // @param {Number} N rotation to apply, default 13
  * @return {String} encoded with ROT13
  */
-function encode(phrase/*, N */){
-    // YOUR CODE HERE... EMPHASIS ON **YOUR**
+function encode(phrase, N){
+  // YOUR CODE HERE... EMPHASIS ON **YOUR**
+  var secretCode = phrase.split(''); //Turning the string that was entered as a parameter into an array of characters.
+  for (var i = 0; i < secretCode.length; i++) {
+    secretCode[i] = secretCode[i].charCodeAt() + N; //Trying to get the Unicode number for the current character to determine shift.
+    if (secretCode[i] > 122) { //evaluating the Unicode number against the Unicode number for Z.
+      secretCode[i] = String.fromCharCode(secretCode[i] - 26); //Taking that Unicode number, doing the math I think it needs to move 13 characters and converting that back to a character.
+    } else {
+      secretCode[i] = String.fromCharCode(secretCode[i]);
+    }
+  }
+  return secretCode.join('');
 }
+
+//I really have no clue how to write this function but have spent around 3 hours researching, looking up the methods associated with strings and arrays, and trying to glean help from my peers.
 
 /**
  * Function `decode` accepts a `phrase` and `N` and
