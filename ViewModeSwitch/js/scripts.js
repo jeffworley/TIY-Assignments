c4db23949da84acfd9dac134fd104911ad0a78df
jeffworley <sirwhirls@gmail.com>
2015-09-11 16:07:50 -0400

    Added eventListeners for each icon <a> element that manipulate the classes to change the view of the information

--- ViewModeSwitch/js/scripts.js
+++ ViewModeSwitch/js/scripts.js
@@ -9,6 +9,49 @@
  * Target the anchor tags within the div element block with an id of "cbp-vm" and classes of "cbp-vm-switcher" and "cbp-vm-view-grid".
  */
 
+// To access the <a> element with a fixed class of 'cbp-vm-list'.
+var listBtn = document.querySelector('.cbp-vm-list');
+
+// To assign an event listener to the <a> element with fixed class of 'cbp-vm-list' with trace bullet message.
+listBtn.addEventListener('click', function(){
+
+  // Tracer bullet message.
+  //console.log('You clicked the list icon!!!');
+
+  // Add the class 'cbp-vm-selected' when click event is heard.
+  listBtn.classList.add('cbp-vm-selected');
+  // And remove the class 'cbp-vm-selected' from the gridBtn <a> element.
+  gridBtn.classList.remove('cbp-vm-selected');
+  // Then remove the class 'cbp-vm-view-grid' from the <div> element with a fixed class of 'cbp-vm-switcher'.
+  switcher.classList.remove('cbp-vm-view-grid');
+  // Then add the class of 'cbp-vm-view-list' to the <div> element with a fixed class of 'cbp-vm-switcher'.
+  switcher.classList.add('cbp-vm-view-list');
+
+});
+
+// To access the <a> element with a fixed class of 'cbp-vm-grid'.
+var gridBtn = document.querySelector('.cbp-vm-grid');
+
+// To assign an event listener to the <a> element with a fixed class of 'cbp-vm-grid' with a tracer bullet message.
+gridBtn.addEventListener('click', function(){
+
+  // Tracer bullet message.
+  //console.log('You clicked the grid icon!!!');
+
+  // Add the class 'cbp-vm-selected' when click event is heard.
+  gridBtn.classList.add('cbp-vm-selected');
+  // And remove the class 'cbp-vm-selected' from the listBtn <a> element.
+  listBtn.classList.remove('cbp-vm-selected');
+  // Then remove the class 'cbp-vm-view-list' from the <div> element with a fixed class of 'cbp-vm-switcher'.
+  switcher.classList.remove('cbp-vm-view-list');
+  // Then add the class 'cbp-vm-view-grid' from the <div> element with a fixed class of 'cbp-vm-switcher'.
+  switcher.classList.add('cbp-vm-view-grid');
+  
+});
+
+// To access the <div> element with a fixed class of 'cbp-vm-switcher'.
+var switcher = document.querySelector('.cbp-vm-switcher');
+
 /*
  * Associate the click action on the anchor element with data-view specified as "cbp-vm-view-list" and have it reassign the class "cbp-vm-selected" from the anchor element with data-view specified as "cbp-vm-view-grid" to itself.
  */
