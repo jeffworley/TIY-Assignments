1169fb062b79c84ef2eba9e38f63485baf7930db
jeffworley <sirwhirls@gmail.com>
2015-09-27 14:14:35 -0400

    Compiled styles.scss to css, linked jquery to index.html and converted vanilla javascript in scripts.js to jquery javascript

--- ViewModeSwitch/js/scripts.js
+++ ViewModeSwitch/js/scripts.js
@@ -1,14 +1,4 @@
-// Notes on what steps I think I need to take in order to code the correct actions for the ViewModeSwitch Blueprint project.
-
-// What does it do?
 /*
- * This webpage displays set groups of related information on wine in either a grid format or list format.  From what I can gather the overall structure and content of this webpage is set in the index.html file and the simple minimalist stylings are controlled by the CSS files.  The main function of the js file I hope to create will be to create the click action functions that toggle the display of the information between the grid view and list view.
-
-// What code do I need to write?  What dom elements do I need to access and manipulate in order for the code to have it's desired effect?
-/*
- * Target the anchor tags within the div element block with an id of "cbp-vm" and classes of "cbp-vm-switcher" and "cbp-vm-view-grid".
- */
-
 // To access the <a> element with a fixed class of 'cbp-vm-list'.
 var listBtn = document.querySelector('.cbp-vm-list');
 
@@ -28,6 +18,7 @@ listBtn.addEventListener('click', function(){
   switcher.classList.add('cbp-vm-view-list');
 
 });
+
 
 // To access the <a> element with a fixed class of 'cbp-vm-grid'.
 var gridBtn = document.querySelector('.cbp-vm-grid');
@@ -51,19 +42,40 @@ gridBtn.addEventListener('click', function(){
 
 // To access the <div> element with a fixed class of 'cbp-vm-switcher'.
 var switcher = document.querySelector('.cbp-vm-switcher');
+*/
 
-/*
- * Associate the click action on the anchor element with data-view specified as "cbp-vm-view-list" and have it reassign the class "cbp-vm-selected" from the anchor element with data-view specified as "cbp-vm-view-grid" to itself.
- */
+// Create functionality for list button.
+$(".cbp-vm-list").on('click', function() {
+  // List Button Tracer Bullet!!!
+  console.log("List Button Clicked!!!");
 
-/*
- * At the same time have the clock action on the anchor element toggle of the class on the div element with id "cbp-vm" to the current view class.  In this case "cbp-vm-view-list".
- */
+  // Remove the selected class from grid button.
+  $(".cbp-vm-grid").removeClass("cbp-vm-selected");
 
-/*
- * Figure out how the above class change and reassignment affects the display output.
- */
+  // Add the selected class to the list button.
+  $(".cbp-vm-list").addClass("cbp-vm-selected");
 
-/*
- * Make sure any changes can be toggled between anchor element click actions.
- */
+  // Apply the correct class to the <div> element surrounding the buttons.
+  $(".cbp-vm-switcher").removeClass("cbp-vm-view-grid");
+
+  $(".cbp-vm-switcher").addClass("cbp-vm-view-list");
+
+});
+
+// Create functionality for grid button.
+$(".cbp-vm-grid").on('click', function() {
+  // Grid Button Tracer Bullet!!!
+  console.log("Grid Button Clicked!!!");
+
+  // Remove the selected class from the list button.
+  $("cbp-vm-list").removeClass("cbp-vm-selected");
+
+  // Add the selected class to the grid button.
+  $("cbp-vm-grid").addClass("cbp-vm-selected");
+
+  // Apply the correct class to the <div> element surrounding the buttons.
+  $(".cbp-vm-switcher").removeClass("cbp-vm-view-list");
+
+  $(".cbp-vm-switcher").addClass("cbp-vm-view-grid");
+  
+})
